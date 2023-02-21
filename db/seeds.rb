a3903d3f14f81154173dd7e06b8cb3a4762b1694
Anne Ly <anne.ly59@gmail.com>
2023-02-21 14:12:20 +0100

    create edit destroy search

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Destroy previous seeds"
+Flat.destroy_all
+
+puts "Creating new seeds with Faker"
+
+puts "Ceating some flats"
+
+20.times do
+  flat = Flat.create!(
+    name: Faker::TvShows::Friends.location,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(1..10),
+    picture_url: 'https://images.unsplash.com/photo-1591825729269-caeb344f6df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  )
+  puts "Your flat : #{flat.name} has been created"
+end
